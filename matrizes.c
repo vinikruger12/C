fcc53930caa8957def01fd6f97995737f011cd2d
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2025-06-09 15:38:09 -0300

    c

---
+++ matrizes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int matriz[3][3];
+
+    for(int i = 0;i < 3;i++){
+        for(int j = 0; j < 3;j++){
+            scanf("%i", &matriz[i][j]);
+        }
+    }
+
+    for(int i = 0;i < 3;i++){
+        for(int j = 0; j < 3;j++){
+            printf("%i  ", matriz[i][j]);
+        }
+              printf("\n");
+    }
+
+
+
+}
